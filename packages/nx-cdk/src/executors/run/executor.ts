ad000d114851811079bf4955cf624ff186ae8055
Christopher Fraser <chris@otterdev.io>
2021-08-13 08:19:38 +0800

    fix out dirs in cdk runner

--- packages/nx-cdk/src/executors/run/executor.ts
+++ packages/nx-cdk/src/executors/run/executor.ts
@@ -1,17 +1,25 @@
 import { CDKRunExecutorSchema } from './schema';
-import { ExecutorContext, getPackageManagerCommand } from '@nrwl/devkit';
+import {
+  ExecutorContext,
+  getPackageManagerCommand,
+  offsetFromRoot,
+} from '@nrwl/devkit';
 import { spawnSync } from 'child_process';
+import path = require('path');
 
 export default async function runExecutor(
   options: CDKRunExecutorSchema,
   context: ExecutorContext
 ) {
+  const projectRoot = context.workspace.projects[context.projectName].root;
+  const outPath = path.join(offsetFromRoot(projectRoot), options.outputPath);
+
   const out = spawnSync(
     `${getPackageManagerCommand().exec} cdk ${options.command} ${
       options.parameters?.join(' ') ?? ''
-    } ${options.options ?? ''} -o ${options.outputPath}`,
+    } ${options.options ?? ''} -o ${outPath}`,
     {
-      cwd: context.workspace.projects[context.projectName].root,
+      cwd: projectRoot,
       shell: true,
       stdio: 'inherit',
     }
