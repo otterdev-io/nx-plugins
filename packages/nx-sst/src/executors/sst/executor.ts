64a2f69233326d80b538cf19e43c935937f1dd64
Chris Fraser <chris@otterdev.io>
2021-09-03 02:46:28 +0800

    Remove output path config

--- packages/nx-sst/src/executors/sst/executor.ts
+++ packages/nx-sst/src/executors/sst/executor.ts
@@ -1,6 +1,5 @@
 import { SSTRunExecutorSchema } from './schema';
-import { ExecutorContext, offsetFromRoot } from '@nrwl/devkit';
-import path = require('path');
+import { ExecutorContext } from '@nrwl/devkit';
 import { runExecutor as execExecutor } from '@otterdev/nx-exec';
 
 export default async function runExecutor(
@@ -8,7 +7,6 @@ export default async function runExecutor(
   context: ExecutorContext
 ) {
   const projectRoot = context.workspace.projects[context.projectName].root;
-  const outPath = path.join(offsetFromRoot(projectRoot), options.outputPath);
   const stage = options.stage ? `--stage ${options.stage}` : '';
   const region = options.region ? `--region ${options.region}` : '';
   const roleArn = options.roleArn ? `--role-arn ${options.roleArn}` : '';
