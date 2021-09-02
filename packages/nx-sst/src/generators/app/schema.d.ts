6998f610c4f9dd5ea923b338fe76792e53d6e470
Christopher Fraser <chris@otterdev.io>
2021-09-03 02:35:03 +0800

    work on nx-sst

--- packages/nx-sst/src/generators/app/schema.d.ts
+++ packages/nx-sst/src/generators/app/schema.d.ts
@@ -2,6 +2,7 @@ import { InitSchema } from '../init/schema';
 
 export interface AppGeneratorSchema extends InitSchema {
   name: string;
+  region?: string;
   tags?: string;
   directory?: string;
 }
