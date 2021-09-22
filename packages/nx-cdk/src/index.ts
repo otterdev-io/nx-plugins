d13e0d29597675ea257e2c5d4ceac78a76f55258
Chris Fraser <chris@otterdev.io>
2021-09-22 09:49:08 +0800

    Export generator and executor

---
+++ packages/nx-cdk/src/index.ts
@@ -0,0 +1,6 @@
+import cdkAppGenerator from './generators/app/generator';
+import cdkRunExecutor from './executors/run/executor';
+
+export { cdkAppGenerator, cdkRunExecutor };
+export * from './generators/app/schema';
+export * from './executors/run/schema';
